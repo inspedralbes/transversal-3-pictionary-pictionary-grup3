5898a888cb751afb2ee0f67505b7011824acc0cb
ArnauDrets <a21arndrelli@inspedralbes.cat>
2023-03-07 13:15:45 +0100

    Socket Client

--- web/front/src/components/teacher/CreateGame.js
+++ web/front/src/components/teacher/CreateGame.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io.connect('https://localhost:7500');
 
 const CreateGame = () => {
+  const [room, setRoom] = useState('');
+  const [users, setUsers] = useState(5);
 
-    function codeGenerator(long) {
-        let items = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        let code = "";
-        for (let i = 0; i < long; i++) {
-            code += items.charAt(Math.floor(Math.random() * items.length));
-        }
-        return code;
+  useEffect(() => {
+    codeGenerator(10);
+  }, []);
+
+  function codeGenerator(long) {
+    let items =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let code = '';
+    for (let i = 0; i < long; i++) {
+      code += items.charAt(Math.floor(Math.random() * items.length));
     }
+    setRoom(code);
+  }
 
-    return (
-        <div>
-            <label>Here's the code to your lobby! Have FUN!</label>
-            <p id="code">{codeGenerator(10)}</p>
-        </div>
-    );
+  const createNewLobby = () => {
+    socket.emit('new lobby', { lobby_code: room, maxUsers: users });
+  };
+
+  return (
+    <div>
+      <label>Here's the code to your lobby! Have FUN!</label>
+      <p id='code'>{room}</p>
+      <button onClick={createNewLobby}>Create a new lobby</button>
+    </div>
+  );
 };
 
 export default CreateGame;
