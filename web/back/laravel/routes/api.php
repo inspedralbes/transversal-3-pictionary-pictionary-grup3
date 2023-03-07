0eb0f051089bb5d038bc00d8e17061a49e500702
Arnau Català <a22arncatsan@inspedralbes.cat>
2023-03-07 11:46:31 +0100

    categories i words

--- web/back/laravel/routes/api.php
+++ web/back/laravel/routes/api.php
@@ -7,12 +7,13 @@ use App\Http\Controllers\Api\WordController;
 
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
-Route::get('list-categories', [CategoryController::class, 'listCategories']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('logout', [UserController::class, 'logout']);
     Route::post('update-profile', [UserController::class, 'updateProfile']);
     Route::get('user-profile', [UserController::class, 'userProfile']);
     Route::post('create-category', [CategoryController::class, 'createCategory']);
+    Route::get('list-categories', [CategoryController::class, 'listCategories']);
     Route::post('create-word', [WordController::class, 'createWord']);
+    Route::post('list-words', [WordController::class, 'listWords']);
 });
