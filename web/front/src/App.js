1b917c85e52c4aae0d3865666608fdd9c1ec35af
ArnauDrets <a21arndrelli@inspedralbes.cat>
2023-03-08 12:20:51 +0100

    Routes

--- web/front/src/App.js
+++ web/front/src/App.js
@@ -15,11 +15,11 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Landing />} socket={socket} />
-        <Route path='/login' element={<Login />} socket={socket} />
-        <Route path='/register' element={<Register />} socket={socket} />
-        <Route path='/createGame' element={<CreateGame />} socket={socket} />
-        <Route path='/joinGame' element={<JoinGame />} socket={socket} />
+        <Route path='/' element={<Landing  socket={socket} />} />
+        <Route path='/login' element={<Login  socket={socket}/>  }/>
+        <Route path='/register' element={<Register socket={socket} /> }/>
+        <Route path='/createGame' element={<CreateGame socket={socket} /> }/>
+        <Route path='/joinGame' element={<JoinGame socket={socket} />  }/>
       </Routes>
     </Router>
   );
