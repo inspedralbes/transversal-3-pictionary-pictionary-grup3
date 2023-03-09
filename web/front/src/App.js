033e105c7fa41998ae38f8d9b0341d15e77db86e
CarlosGomezFuentes <a21cargomfue@inspedralbes.cat>
2023-03-09 12:55:51 +0100

    Diseño login hecho

--- web/front/src/App.js
+++ web/front/src/App.js
@@ -3,9 +3,9 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 import './style/style.css';
 import Landing from './pages/Landing';
-import Login from './Components/teacher/Login';
-import Register from './Components/teacher/Register';
-import CreateGame from './Components/teacher/CreateGame';
+import Login from './components/teacher/Login';
+import Register from './components/teacher/Register';
+import CreateGame from './components/teacher/CreateGame';
 
 export const router = createBrowserRouter([
   {
