9e2ef715bf46e1845da68d1639b2b36ca0e7de58
ArnauDrets <a21arndrelli@inspedralbes.cat>
2023-03-09 13:11:27 +0100

    Lobbies Socket

--- web/front/src/components/teacher/CreateGame.js
+++ web/front/src/components/teacher/CreateGame.js
@@ -1,14 +1,19 @@
-import React, { useEffect, useState, useRef } from 'react';
-import io from 'socket.io-client';
+import React, { useEffect, useState } from 'react';
 
 const CreateGame = ({ socket }) => {
   const [room, setRoom] = useState(null);
-  const [users, setUsers] = useState(5);
-  const canvasRef = useRef(null);
+  const [users, setUsers] = useState(null);
+  const [lobbies, setLobbies] = useState([]);
 
   useEffect(() => {
     codeGenerator();
+    userGenerator();
   }, []);
+
+  const userGenerator = () => {
+    const randomUsers = Math.floor(Math.random() * (5 - 1 + 1) + 1);
+    setUsers(randomUsers);
+  };
 
   const codeGenerator = () => {
     const randomCode = Math.floor(
@@ -19,7 +24,7 @@ const CreateGame = ({ socket }) => {
 
   const createNewLobby = () => {
     codeGenerator();
-    console.log(room);
+    userGenerator();
     socket.emit('new lobby', {
       lobby_code: room,
       maxUsers: users,
@@ -28,27 +33,42 @@ const CreateGame = ({ socket }) => {
   };
 
   const getLobby = () => {
-    socket.emit('get lobbies', () => {
-      socket.on('lobbies list', (data) => {
-        console.log(data);
-      });
+    socket.emit('get lobbies', {});
+    socket.on('lobbies list', function (data) {
+      setLobbies(data);
     });
   };
 
-  function wipe() {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-  }
+  const showLobby = () => {
+    console.log(lobbies);
+  };
 
   return (
-    <div>
-      <label>Here's the code to your lobby! Have FUN!</label>
-      <p id='code'>{room}</p>
-      <button onClick={createNewLobby}>Create a new lobby</button>
-      <hr></hr>
-      <button onClick={getLobby}>Get Lobby</button>
-    </div>
+    <>
+      <div>
+        <label>Here's the code to your lobby! Have FUN!</label>
+        <p id='code'>{room}</p>
+        <button onClick={createNewLobby}>Create a new lobby</button>
+        <hr></hr>
+        <button onClick={getLobby}>Get Lobby</button>
+        <hr></hr>
+        <button onClick={showLobby}>Show Lobby</button>
+      </div>
+      <div className='lobby-list'>
+        {lobbies.map((lobby, index) => (
+          <div className='lobby' key={index}>
+            <h2>{lobby.lobby_code}</h2>
+            <p>{lobby.category}</p>
+            {lobby.users.map((user, index) => (
+              <span className='users' key={index}>
+                USUARIO: {user.name}
+              </span>
+            ))}
+            <p>{lobby.maxUsers}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
