c62bb31a8bc5aedb3f8e286faa0a4653f7cd66a6
ArnauDrets <a21arndrelli@inspedralbes.cat>
2023-03-09 10:04:58 +0100

    useEffect

--- web/front/src/components/teacher/CreateGame.js
+++ web/front/src/components/teacher/CreateGame.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from 'react';
 
-const CreateGame = ({socket}) => {
+const CreateGame = ({ socket }) => {
   const [room, setRoom] = useState(null);
   const [users, setUsers] = useState(5);
 
+  useEffect(() => {
+    codeGenerator();
+  }, []);
+
   const codeGenerator = () => {
-    const randomCode = Math.floor(Math.random() * (100000 - 999999 + 1) + 999999);
+    const randomCode = Math.floor(
+      Math.random() * (100000 - 999999 + 1) + 999999
+    );
     setRoom(randomCode);
-  }
+  };
 
   const createNewLobby = () => {
-    codeGenerator()
-    console.log(room)
-    socket.emit('new lobby', { lobby_code: room, maxUsers: users, category: 'Info'});
+    codeGenerator();
+    console.log(room);
+    socket.emit('new lobby', {
+      lobby_code: room,
+      maxUsers: users,
+      category: 'Info',
+    });
   };
 
   return (
