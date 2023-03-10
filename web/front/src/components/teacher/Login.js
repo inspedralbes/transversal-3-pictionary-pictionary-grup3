9844ce0ca38e9dec6bc8869c0e8b5334b3386885
CarlosGomezFuentes <a21cargomfue@inspedralbes.cat>
2023-03-10 09:37:37 +0100

    Diseño login y register terminado

--- web/front/src/components/teacher/Login.js
+++ web/front/src/components/teacher/Login.js
@@ -3,6 +3,7 @@ import logo from '../../style/logoPictoboom small.png';
 import "../../style/style.css"
 import { Link } from "react-router-dom";
 
+
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -58,8 +59,8 @@ const Login = () => {
         <button type='submit'>Login</button>
       </form> */}
 
-      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md space-y-8">
+      <div className="flex mt-20 h-50 w-1/2 py-12 px-4 sm:px-6 lg:px-8 m-[auto]  border-rose-600 rounded-lg bg-rose-50 shadow-2xl">
+        <div className="w-full max-w-md space-y-8 m-[auto]">
             <div>
                 <a href='/'><img className="mx-auto h-20 w-auto" src={logo} alt="Your Company"></img></a>
                 <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
@@ -70,11 +71,11 @@ const Login = () => {
                 <div className="-space-y-px rounded-md shadow-sm">
                     <div>
                         <label htmlFor="username-address" className="sr-only">Username</label>
-                        <input id="username-address" name="username" type="username" value={username} onChange={handlerChangeUsername} autoComplete="username" required className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="username address"></input>
+                        <input id="username-address" name="username" type="username" value={username} onChange={handlerChangeUsername} autoComplete="username" required className="relative block w-full p-3 rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-rose-600 sm:text-sm sm:leading-6" placeholder="username address"></input>
                     </div>
                     <div>
                         <label htmlFor="password" className="sr-only">Password</label>
-                        <input id="password" name="password" type="password" value={password} onChange={handlerChangePassword} autoComplete="current-password" required className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password"></input>
+                        <input id="password" name="password" type="password" value={password} onChange={handlerChangePassword} autoComplete="current-password" required className="relative block w-full p-3 rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-rose-600 sm:text-sm sm:leading-6" placeholder="Password"></input>
                     </div>
                 </div>
 
@@ -85,7 +86,7 @@ const Login = () => {
                     </div>
 
                     <div className="text-sm">
-                        <a href="#" className="font-medium text-rose-800 hover:text-indigo-500">Forgot your password?</a>
+                        <Link to="/register" className="font-medium text-rose-800 hover:text-indigo-500">Havn't an account yet?</Link>
                     </div>
                 </div>
 
--- web/front/src/components/teacher/Register.js
+++ web/front/src/components/teacher/Register.js
@@ -28,38 +28,33 @@ const Register = () => {
         <button type='submit'>Login</button>
       </form> */}
 
-      {/* <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 border-solid border-2 border-rose-600 w-96 shadow-2xl bg-rose-50"> */}
-      <div className="flex mt-20 h-50 w-1/2 py-12 px-4 sm:px-6 lg:px-8 m-[auto]  border-rose-600 rounded-lg bg-rose-50 shadow-2xl">
-        <div className="w-full max-w-md space-y-8  m-[auto]">
+      <div className="flex h-50 w-1/2 py-12 px-4 sm:px-6 lg:px-8 m-[auto] border-rose-600 rounded-lg bg-rose-50 shadow-2xl">
+        <div className="w-full max-w-md space-y-8 m-[auto]">
             <div>
                 <a href='/'><img className="mx-auto h-20 w-auto" src={logo} alt="Your Company"></img></a>
-                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Register your account</h2>
                 
             </div>
             <form className="mt-8 space-y-6" action="#" method="POST">
                 <input type="hidden" name="remember" value="true"></input>
                 <div className="-space-y-px rounded-md shadow-sm">
                     <div>
-                        <label htmlFor="username-address" className="sr-only">Username</label>
-                        <input id="username-address" name="username" type="username" autoComplete="username" required className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="username address"></input>
+                        <label htmlFor="username" className="sr-only">Username</label>
+                        <input id="username" name="username" type="username" required className="relative block w-full border-0 py-1.5 p-3 rounded-md text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Username"></input>
+                    </div>
+                    <div>
+                        <label htmlFor="email-address" className="sr-only">Email</label>
+                        <input id="email-address" name="email" type="email" required className="relative block w-full border-0 py-1.5 mt-2 p-3 rounded-md text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email address"></input>
                     </div>
                     <div>
                         <label htmlFor="password" className="sr-only">Password</label>
-                        <input id="password" name="password" type="password" autoComplete="current-password" required className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password"></input>
+                        <input id="password" name="password" type="password" required className="relative block w-full border-0 py-1.5 mt-6 p-3 rounded-md text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password"></input>
+                    </div>
+                    <div>
+                        <label htmlFor="password-confirmation" className="sr-only">Password</label>
+                        <input id="password-confirmation" name="password-confirmation" type="password" required className="relative block w-full border-0 py-1.5 mt-2 p-3 rounded-md text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password"></input>
                     </div>
                 </div>
-
-                <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                        <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"></input>
-                        <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">Remember me</label>
-                    </div>
-
-                    <div className="text-sm">
-                        <a href="#" className="font-medium text-rose-800 hover:text-indigo-500">Forgot your password?</a>
-                    </div>
-                </div>
-
                 <div>
                     <button type="submit" className="group relative flex w-full justify-center rounded-md bg-rose-500 py-2 px-3 text-sm font-semibold text-white hover:pink-to-orange-gr focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                         <span className="absolute inset-y-0 left-0 flex items-center pl-3">
@@ -67,7 +62,7 @@ const Register = () => {
                                 <path fillRule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clipRule="evenodd" />
                             </svg>
                         </span>
-                        Sign in
+                        Register
                     </button>
                 </div>
             </form>
