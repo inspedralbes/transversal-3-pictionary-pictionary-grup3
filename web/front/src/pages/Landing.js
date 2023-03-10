655cbab1ab281fae8202d59457441e960ccda6e5
Arnau Català <a22arncatsan@inspedralbes.cat>
2023-03-10 10:43:01 +0100

    fix header

--- web/front/src/pages/Landing.js
+++ web/front/src/pages/Landing.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import '../style/style.css';
-// import Header from '../components/Header';
+import Header from '../components/Header';
 import Body from '../components/bodyLanding';
 
 const Landing = () => {
   return (
     <>
+      <Header></Header>
       <Body></Body>
     </>
   );
