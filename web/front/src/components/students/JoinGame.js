7a649d0031f03c3df379bcc3f69c8295221e02bd
ArnauDrets <a21arndrelli@inspedralbes.cat>
2023-03-13 10:39:26 +0100

    Salas sockets

--- web/front/src/components/students/JoinGame.js
+++ web/front/src/components/students/JoinGame.js
@@ -6,35 +6,46 @@ import { v4 as uuidv4 } from 'uuid';
 const JoinGame = ({ socket }) => {
   const [nameUser, setNameUser] = useState('');
   const [lobbyCode, setLobbyCode] = useState('');
-  const [lobbies, setLobbies] = useState([]);
+  const [correctName, setCorrectName] = useState(true);
+  const [correctLobby, setCorrectLobby] = useState(true);
 
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    socket.emit('join room', {
-      name: nameUser,
-      userId: uuidv4(),
-      lobby_code: lobbyCode,
-    });
-    checkCodeRoom();
+    if (nameUser != '') {
+      setCorrectName(true);
+      socket.emit('join room', {
+        name: nameUser,
+        userId: uuidv4(),
+        lobby_code: lobbyCode,
+      });
+      checkCodeRoom();
+    } else {
+      setCorrectName(false);
+      console.log('nombreVacio');
+    }
   };
 
   const checkCodeRoom = () => {
+    let lobbies = {};
     socket.emit('get lobbies', {});
     socket.on('lobbies list', function (data) {
-      setLobbies(data);
-    });
+      lobbies = data;
 
-    for (let i = 0; i < lobbies.length; i++) {
-      if (lobbies[i].lobby_code === parseInt(lobbyCode)) {
-        if (lobbies[i].users.length < lobbies[i].maxUsers) {
-          navigate('/playGame');
-        } else {
-          alert('lobby llena');
+      for (let i = 0; i < lobbies.length; i++) {
+        if (lobbies[i].lobby_code === parseInt(lobbyCode)) {
+          console.log(lobbyCode);
+          if (lobbies[i].users.length < lobbies[i].maxUsers) {
+            navigate('/playGame');
+          } else {
+            alert('lobby llena');
+          }
         }
       }
-    }
+
+      setCorrectLobby(false);
+    });
   };
 
   return (
@@ -59,6 +70,16 @@ const JoinGame = ({ socket }) => {
             Send
           </button>
         </form>
+        {!correctName && (
+          <div>
+            <p>Fill in the name field please!</p>
+          </div>
+        )}
+        {!correctLobby && (
+          <div>
+            <p>The lobby is non-existent!</p>
+          </div>
+        )}
       </div>
     </div>
   );
