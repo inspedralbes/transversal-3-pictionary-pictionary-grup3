3ffe76a79f10a6a249493fc8f53c4dab244a5fde
a21ismalgelo <a21ismalgelo@inspedralbes.cat>
2023-03-13 13:47:35 +0100

    disseny inicial amb cards CreateGame.js

--- web/front/src/components/teacher/CreateGame.js
+++ web/front/src/components/teacher/CreateGame.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import '../../style/style.css';
+import React, { useEffect, useState } from "react";
+import "../../style/style.css";
 
 const CreateGame = ({ socket }) => {
   const [room, setRoom] = useState(null);
@@ -26,16 +26,16 @@ const CreateGame = ({ socket }) => {
   const createNewLobby = () => {
     codeGenerator();
     userGenerator();
-    socket.emit('new lobby', {
+    socket.emit("new lobby", {
       lobby_code: room,
       maxUsers: users,
-      category: 'Info',
+      category: "Info",
     });
   };
 
   const getLobby = () => {
-    socket.emit('get lobbies', {});
-    socket.on('lobbies list', function (data) {
+    socket.emit("get lobbies", {});
+    socket.on("lobbies list", function (data) {
       setLobbies(data);
     });
   };
@@ -48,27 +48,50 @@ const CreateGame = ({ socket }) => {
     <>
       <div>
         <label>Here's the code to your lobby! Have FUN!</label>
-        <p id='code'>{room}</p>
+        <p id="code">{room}</p>
         <button onClick={createNewLobby}>Create a new lobby</button>
         <hr></hr>
         <button onClick={getLobby}>Get Lobby</button>
         <hr></hr>
         <button onClick={showLobby}>Show Lobby</button>
       </div>
-      <div className='lobby-list'>
+      <div className="grid grid-cols-3 gap-2 items-center justify-center">
         {lobbies.map((lobby, index) => (
-          <div className='lobby' key={index}>
-            <h2>{lobby.lobby_code}</h2>
-            <p>{lobby.category}</p>
-            {lobby.users.map((user, index) => (
-              <span className='users' key={index}>
-                USUARIO: {user.name}
-              </span>
-            ))}
-            <p>{lobby.maxUsers}</p>
+        <div className="h-36 w-full rounded-md pink-to-orange-gr p-1" key={index}>
+          <div className="h-full w-full bg-gray-200 back">
+            <div className="max-w-sm rounded overflow-hidden" key={index}>
+              <div className="">
+                <div className="font-bold text-gray-800 text-xl">{lobby.lobby_code}</div>
+                <p className="text-gray-800 text-base">{lobby.category}</p>
+                </div>
+                <div className="">
+                  {lobby.users.map((user, index) => (
+                  <span className="users" key={index}>
+                    {user.name}
+                  </span>
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
         ))}
       </div>
+          
+            {/* <div classNameName="lobby-list text-gray-100">
+              {lobbies.map((lobby, index) => (
+                <div classNameName="lobby" key={index}>
+                  <h2>{lobby.lobby_code}</h2>
+                  <p>{lobby.category}</p>
+                  {lobby.users.map((user, index) => (
+                    <span classNameName="users" key={index}>
+                    USUARIO: {user.name}
+                    </span>
+                    ))}
+                    <p>{lobby.maxUsers}</p>
+                    </div>
+                    ))}
+                  </div> */}
+
     </>
   );
 };
