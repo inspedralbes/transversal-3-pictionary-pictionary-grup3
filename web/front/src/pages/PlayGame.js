194aa147d73ccfbd1e82bb20e31e12754e94bb3a
CarlosGomezFuentes <a21cargomfue@inspedralbes.cat>
2023-03-15 13:23:57 +0100

    Diseño de playGame mejorado y chat implementado

--- web/front/src/pages/PlayGame.js
+++ web/front/src/pages/PlayGame.js
@@ -22,8 +22,6 @@ const CreateGame = ({ socket }) => {
       console.log(users);
     });
 
-    getParaula();
-
     var tiempoRestante = 100;
 
     function actualizarContador() {
@@ -110,32 +108,95 @@ const CreateGame = ({ socket }) => {
   }
 
   return (
-    <div className="h-screen flex bg-[url('../style/spinning-bg-pinchitos.png')] bg-cover bg-center items-center lg:bg-[url('../style/webBackground.png')]">
-      <div className="w-screen flex items-center justify-center">
-        <div className='w-fit'>
-          <div className="flex items-center">
-            <div>
-              <div id="contador"></div>
-              <div>
-                <input onChange={changeColor} type='color' id='colorPicker' />
-                <input
-                  onClick={changeBrush}
-                  type='range'
-                  min='1'
-                  max='20'
-                  id='brushSize'
-                />
-                <label id='brushText'>Brush Size: {brushSize} </label>
-                <button onClick={wipe}>Wipe</button>
-              </div>
-              <div>
-                <canvas ref={canvasRef} width='600px' height='600px' className="bg-white"></canvas>
-              </div>
+        <div className="flex bg-cover bg-center lg:bg-fixed bg-[url('../style/webBackground.png')]">
+            <div className="relative w-full max-w-screen-lg mx-auto">
+                <div className="absolute inset-0 z-[-1] bg-cover bg-center" style={{ backgroundImage: "url('../style/spinning-bg-pinchitos.png')" }}></div>
+                <div className="px-4 py-12 mt-10 mx-auto max-w-xl border-4 border-rose-300 bg-rose-100 shadow-2xl rounded-lg">
+                    <div className="flex items-center justify-center mb-6">
+                        <div id="contador" className="w-16 h-16 mb-10 rounded-full bg-red-500 flex items-center justify-center text-white font-bold text-lg"></div>
+                    </div>
+                    <h2 className="text-2xl font-bold tracking-tight text-gray-900 mb-6">Choose your colors   
+                        <button 
+                            onClick={wipe} 
+                            className="ml-4 px-3 py-1 rounded text-white bg-rose-500 hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500">
+                            Wipe
+                        </button>
+                    </h2>
+                    <div className="flex items-center mb-4">
+                        <label htmlFor="colorPicker" className="mr-4">Color:</label>
+                        <input 
+                            onChange={changeColor} 
+                            type="color" 
+                            id="colorPicker" 
+                            className="h-8 w-8" 
+                        />
+                    </div>
+                    <div className="flex items-center mb-6">
+                        <label htmlFor="brushSize" className="mr-4">Brush Size:</label>
+                        <input 
+                            onChange={changeBrush} 
+                            type="range"
+                            min="1" max="20" 
+                            id="brushSize" 
+                            className="h-4 w-48"
+                        />
+                        <span className="ml-4 text-gray-700">{brushSize}</span>
+                    </div>
+                </div>
+                <div className="mt-12 mx-auto max-w-xl">
+                    <div id="canvas" className="flex items-center justify-center">
+                        <canvas 
+                            ref={canvasRef} 
+                            width="750px" height="600px" 
+                            className="bg-white border-4 border-rose-300">
+                        </canvas>
+                    </div>
+                    <div id="chat" className="flex items-center justify-center mt-12">
+                        ño
+                    </div>
+                </div>
             </div>
-          </div>
+
+            {/* CHAT */}
+
+
+            <div id="chat" class="mt-12 mx-auto max-w-xl">
+                <div class="h-64 overflow-y-scroll border-4 border-rose-300 bg-white p-4">
+                    <div class="flex flex-col gap-2">
+                        {/* <!-- Ejemplo de mensaje enviado por el usuario --> */}
+                        <div class="flex flex-col items-end">
+                            <span class="bg-rose-300 text-white rounded-lg px-4 py-2 max-w-xs">
+                                Hola, ¿cómo estás?
+                            </span>
+                            <span class="text-sm text-gray-500 mt-2">Hace 2 minutos</span>
+                        </div>
+
+                        {/* <!-- Ejemplo de mensaje recibido del otro usuario --> */}
+                        <div class="flex flex-col items-start">
+                            <span class="bg-gray-200 rounded-lg px-4 py-2 max-w-xs">
+                                Estoy bien, ¿y tú?
+                            </span>
+                            <span class="text-sm text-gray-500 mt-2">Hace 1 minuto</span>
+                        </div>
+
+                        {/* <!-- Puedes agregar tantos mensajes como desees --> */}
+                    </div>
+                </div>
+
+                {/* <!-- Caja de texto para ingresar un nuevo mensaje --> */}
+                <div class="flex gap-4 mt-4">
+                    <input type="text" placeholder="Escribe un mensaje" class="border-2 border-gray-300 p-2 flex-1"/>
+                    <button class="bg-rose-500 text-white px-4 py-2 rounded-lg hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500">
+                        Enviar
+                    </button>
+                </div>
+            </div>
+
+
         </div>
-      </div>
-    </div>
+  
+        
+    
   );
 };
 
