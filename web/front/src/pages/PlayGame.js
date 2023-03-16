1a2be7dffc13a7a1198f94b593b533366dfaed1d
unknown <a21cargomfue@inspedralbes.cat>
2023-03-16 12:44:12 +0100

    Diseño playGame casi acabado

--- web/front/src/pages/PlayGame.js
+++ web/front/src/pages/PlayGame.js
@@ -110,78 +110,80 @@ const CreateGame = ({ socket }) => {
   return (
         <div className="flex bg-cover bg-center lg:bg-fixed bg-[url('../style/webBackground.png')]">
             <div className="relative w-full max-w-screen-lg mx-auto">
-                <div className="absolute inset-0 z-[-1] bg-cover bg-center" style={{ backgroundImage: "url('../style/spinning-bg-pinchitos.png')" }}></div>
-                <div className="px-4 py-12 mt-10 mx-auto max-w-xl border-4 border-rose-300 bg-rose-100 shadow-2xl rounded-lg">
-                    <div className="flex items-center justify-center mb-6">
-                        <div id="contador" className="w-16 h-16 mb-10 rounded-full bg-red-500 flex items-center justify-center text-white font-bold text-lg"></div>
+                <div 
+                    className="absolute inset-0 z-[-1] bg-cover bg-center" 
+                    style={{ backgroundImage: "url('../style/spinning-bg-pinchitos.png')" }}>
+                </div>
+                <div className="h-64 px-4 py-12 mt-10 mx-auto border-4 border-rose-300 bg-rose-100 shadow-2xl rounded-lg">
+                    <div className="flex items-center justify-center -mt-8">
+                        <div 
+                            id="contador" 
+                            className="w-16 h-16 rounded-full bg-red-500 flex items-center justify-center text-white font-bold text-lg">
+                        </div>
                     </div>
-                    <h2 className="text-2xl font-bold tracking-tight text-gray-900 mb-6">Choose your colors   
-                        <button 
-                            onClick={wipe} 
+                    <h2 className="text-2xl font-bold tracking-tight text-gray-900 mb-6">Choose your colors
+                        <button
+                            onClick={wipe}
                             className="ml-4 px-3 py-1 rounded text-white bg-rose-500 hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500">
                             Wipe
                         </button>
                     </h2>
                     <div className="flex items-center mb-4">
                         <label htmlFor="colorPicker" className="mr-4">Color:</label>
-                        <input 
-                            onChange={changeColor} 
-                            type="color" 
-                            id="colorPicker" 
-                            className="h-8 w-8" 
+                        <input
+                            onChange={changeColor}
+                            type="color"
+                            id="colorPicker"
+                            className="h-8 w-8"
                         />
                     </div>
                     <div className="flex items-center mb-6">
                         <label htmlFor="brushSize" className="mr-4">Brush Size:</label>
-                        <input 
-                            onChange={changeBrush} 
+                        <input
+                            onChange={changeBrush}
                             type="range"
-                            min="1" max="20" 
-                            id="brushSize" 
+                            min="1" max="20"
+                            id="brushSize"
                             className="h-4 w-48"
                         />
                         <span className="ml-4 text-gray-700">{brushSize}</span>
                     </div>
                 </div>
-                <div className="mt-12 mx-auto max-w-xl">
+                <div className="mt-5 mb-5 flex">
                     <div id="canvas" className="flex items-center justify-center">
-                        <canvas 
-                            ref={canvasRef} 
-                            width="750px" height="600px" 
+                        <canvas
+                            ref={canvasRef}
+                            width="700px" height="600px"
                             className="bg-white border-4 border-rose-300">
                         </canvas>
                     </div>
-                </div>
-            </div>
-
-            {/* CHAT */}
-            <div className="absolute">
-                <div id="chat" className="content-end mt-12 m-[auto]">
-                    <div className="h-64 overflow-y-scroll border-4 border-rose-300 bg-white p-4">
-                        <div className="flex flex-col gap-2">
-                            <div className="flex flex-col items-end">
-                                <span className="bg-rose-300 text-white rounded-lg px-4 py-2 max-w-xs">
-                                    Hola, ¿cómo estás?
-                                </span>
-                                <span class="text-sm text-gray-500 mt-2">Hace 2 minutos</span>
-                            </div>
-                            <div className="flex flex-col items-start">
-                                <span className="bg-gray-200 rounded-lg px-4 py-2 max-w-xs">
-                                    Estoy bien, ¿y tú?
-                                </span>
-                                <span className="text-sm text-gray-500 mt-2">Hace 1 minuto</span>
+                    {/* CHAT */}
+                    <div id="chat" className="m-auto w-96 ml-5 -mt-0.5">
+                        <div className="h-96 overflow-y-scroll border-4 border-rose-300 bg-white p-4">
+                            <div className="flex flex-col gap-2">
+                                <div className="flex flex-col items-end">
+                                    <span className="bg-rose-300 text-white rounded-lg px-4 py-2 max-w-xs">
+                                        Hola, ¿cómo estás?
+                                    </span>
+                                    <span class="text-sm text-gray-500 mt-2">Hace 2 minutos</span>
+                                </div>
+                                <div className="flex flex-col items-start">
+                                    <span className="bg-gray-200 rounded-lg px-4 py-2 max-w-xs">
+                                        Estoy bien, ¿y tú?
+                                    </span>
+                                    <span className="text-sm text-gray-500 mt-2">Hace 1 minuto</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="flex gap-4 mt-4">
-                        <input type="text" placeholder="Escribe un mensaje" className="border-2 border-gray-300 p-2 flex-1"/>
-                        <button className="bg-rose-500 text-white px-4 py-2 rounded-lg hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500">
-                            Enviar
-                        </button>
+                        <div className="flex gap-4 mt-4">
+                            <input type="text" placeholder="Escribe un mensaje" className="border-2 border-gray-300 p-2 flex-1"/>
+                            <button className="bg-rose-500 text-white px-4 py-2 rounded-lg hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500">
+                                Enviar
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            
+            </div>  
         </div>
     );
 };
