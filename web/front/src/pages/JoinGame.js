a0337ea75186d21151b660f5839dadde4eab4043
a21ismalgelo <a21ismalgelo@inspedralbes.cat>
2023-03-21 14:11:58 +0100

    codi formatat

--- web/front/src/pages/JoinGame.js
+++ web/front/src/pages/JoinGame.js
@@ -1,15 +1,15 @@
-import '../style/style.css';
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
-import { setUserData } from '../features/dataUserSlice';
+import "../style/style.css";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+import { setUserData } from "../features/dataUserSlice";
 
 export const JoinGame = ({ socket }) => {
-  const [nameUser, setNameUser] = useState('');
-  const [lobbyCode, setLobbyCode] = useState('');
+  const [nameUser, setNameUser] = useState("");
+  const [lobbyCode, setLobbyCode] = useState("");
   const [inLobby, setInLobby] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [usersReady, setUsersReady] = useState([]);
   const [ready, setReady] = useState(false);
   const [currentPlayers, setCurrentPlayers] = useState(0);
@@ -20,27 +20,27 @@ export const JoinGame = ({ socket }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (nameUser !== '' || lobbyCode !== '') {
-      socket.emit('join room', {
+    if (nameUser !== "" || lobbyCode !== "") {
+      socket.emit("join room", {
         name: nameUser,
         userId: uuidv4(),
         lobby_code: parseInt(lobbyCode),
       });
       setInLobby(true);
       dispatch(setUserData(nameUser));
-    }else{
-      setError('Fill in the blanks');
+    } else {
+      setError("Fill in the blanks");
     }
   };
 
   const handleClickLeave = (e) => {
     e.preventDefault();
-    socket.emit('leave lobby');
+    socket.emit("leave lobby");
     setInLobby(false);
   };
 
   const handleClickReady = () => {
-    socket.emit('ready user');
+    socket.emit("ready user");
     setReady(true);
 
     if (ready) {
@@ -49,7 +49,7 @@ export const JoinGame = ({ socket }) => {
   };
 
   useEffect(() => {
-    socket.on('lobby user list', (data) => {
+    socket.on("lobby user list", (data) => {
       const readyUsers = data.list.filter((user) => user.ready);
       const currentPlayers = data.list.length;
       const maxPlayers = data.maxUsers;
@@ -58,8 +58,8 @@ export const JoinGame = ({ socket }) => {
       setUsersReady(readyUsers);
     });
 
-    socket.on('start game', (data) => {
-      navigate('/playGame');
+    socket.on("start game", (data) => {
+      navigate("/playGame");
     });
   });
 
@@ -89,29 +89,29 @@ export const JoinGame = ({ socket }) => {
   return (
     <div className="h-screen flex bg-[url('../style/spinning-bg-pinchitos.png')] bg-cover bg-center items-center justify-center lg:bg-[url('../style/webBackground.png')] w-screen">
       {!inLobby ? (
-        <div className='m-[auto] border-2 rounded-lg w-80 p-16'>
+        <div className="m-[auto] border-2 rounded-lg w-80 p-16">
           <form onSubmit={handleSubmit}>
             <label>
               <input
-                type='text'
+                type="text"
                 value={lobbyCode}
                 onChange={(e) => setLobbyCode(e.target.value)}
-                placeholder='Lobby code'
-                className='input-join'
+                placeholder="Lobby code"
+                className="input-join"
               ></input>
             </label>
             <label>
               <input
-                type='text'
+                type="text"
                 value={nameUser}
                 onChange={(e) => setNameUser(e.target.value)}
-                placeholder='Your name'
-                className='input-join'
+                placeholder="Your name"
+                className="input-join"
               ></input>
             </label>
             <button
-              type='submit'
-              className='default-button font-semibold outline outline-1 p-1 rounded-lg hover:bg-gray-800 hover:text-gray-50'
+              type="submit"
+              className="default-button font-semibold outline outline-1 p-1 rounded-lg hover:bg-gray-800 hover:text-gray-50"
             >
               Send
             </button>
@@ -119,23 +119,40 @@ export const JoinGame = ({ socket }) => {
           </form>
         </div>
       ) : (
-        <div className='flex w-full'>
-          <div className='block'>
-            <button onClick={handleClickLeave} className="default-button m-1">Leave lobby</button>
+        <div className="flex w-full">
+          <div className="block">
+            <button onClick={handleClickLeave} className="default-button m-1">
+              Leave lobby
+            </button>
             {!ready ? (
-              <button onClick={handleClickReady} className="default-button m-1 text-red-700 font-extrabold bg-red-200 red">NOT READY</button>
-              ):(
-                <button onClick={handleClickReady} className="default-button m-1 disabled text-green-900 font-extrabold bg-green-200 disabled:text-green-300 disabled:bg-gray-50" disabled >READY</button>
+              <button
+                onClick={handleClickReady}
+                className="default-button m-1 text-red-700 font-extrabold bg-red-200 red"
+              >
+                NOT READY
+              </button>
+            ) : (
+              <button
+                onClick={handleClickReady}
+                className="default-button m-1 disabled text-green-900 font-extrabold bg-green-200 disabled:text-green-300 disabled:bg-gray-50"
+                disabled
+              >
+                READY
+              </button>
             )}
           </div>
-          <div className='grid grid-cols-4 px-96'>
+          <div className="grid grid-cols-4 px-96">
             {usersReady.map((user) => (
-            <div className='col-span-1 rounded-full bg-pink-50  h-28 w-28'key={user.userId}>{user.name}
-
-            </div>
-                ))}
+              <div
+                className="col-span-1 rounded-full bg-pink-50  h-28 w-28"
+                key={user.userId}
+              >
+                {user.name}
+              </div>
+            ))}
           </div>
         </div>
-        )}
+      )}
     </div>
-)}
+  );
+};
