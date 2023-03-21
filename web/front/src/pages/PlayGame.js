b78f89f458609814f25e84ef4102d82a5a6c2ea2
ArnauDrets <a21arndrelli@inspedralbes.cat>
2023-03-21 13:29:09 +0100

    Diseño incorporado

--- web/front/src/pages/PlayGame.js
+++ web/front/src/pages/PlayGame.js
@@ -25,10 +25,10 @@ export const PlayGame = ({ socket }) => {
   let painterAux = false;
 
   useEffect(() => {
-    console.log('asd')
+    console.log("asd");
     socket.emit("ready user");
     socket.on("start game", (data) => {
-      console.log(data)
+      console.log(data);
       if (data.lobby.painter === nameUser) {
         setPainter(true);
         painterAux = true;
@@ -64,8 +64,8 @@ export const PlayGame = ({ socket }) => {
     return () => clearInterval(interval);
   }, [timer]);
 
-  socket.on('next round', function (data) {
-    console.log('next round', data);
+  socket.on("next round", function (data) {
+    console.log("next round", data);
   });
 
   useEffect(() => {
@@ -76,9 +76,9 @@ export const PlayGame = ({ socket }) => {
     socket.on("correct word", (data) => {
       setUserCorrectWords(data.lobby.users);
     });
-    
-    socket.on('next turn', (data) => {
-      console.log(data)
+
+    socket.on("next turn", (data) => {
+      console.log(data);
       if (data.lobby.painter === nameUser) {
         setPainter(true);
         painterAux = true;
@@ -88,7 +88,7 @@ export const PlayGame = ({ socket }) => {
       }
       setWord(data.lobby.word);
       setRound(data.lobby.round);
-    })
+    });
   });
 
   const handleSubmit = (e) => {
@@ -159,14 +159,14 @@ export const PlayGame = ({ socket }) => {
         context.lineTo(x, y);
         context.strokeStyle = c;
         context.lineWidth = b;
-        context.lineCap = 'round';
+        context.lineCap = "round";
       } else {
         lastX = x;
         lastY = y;
         context.lineTo(x, y);
         context.strokeStyle = c;
         context.lineWidth = b;
-        context.lineCap = 'round';
+        context.lineCap = "round";
       }
       context.fill();
       context.stroke();
@@ -177,7 +177,13 @@ export const PlayGame = ({ socket }) => {
       if (data.data.action == "b") {
         context.clearRect(0, 0, canvas.width, canvas.height);
       } else {
-        draw(data.data.x, data.data.y, data.data.b, data.data.c, data.data.action);
+        draw(
+          data.data.x,
+          data.data.y,
+          data.data.b,
+          data.data.c,
+          data.data.action
+        );
       }
     });
   }, []);
@@ -198,51 +204,18 @@ export const PlayGame = ({ socket }) => {
   }
 
   return (
-    <div className="flex bg-cover bg-center lg:bg-fixed bg-[url('../style/webBackground.png')]">
+    <div className="flex -ml-72 bg-cover bg-center lg:bg-fixed bg-[url('../style/webBackground.png')]">
       <div className="relative w-full max-w-screen-lg mx-auto">
-        {/* <!-- Lista de jugadores --> */}
-        <div class="w-64 bg-white border-4 border-rose-300 shadow-2xl rounded-lg mr-5">
-          <h3 class="text-lg font-bold mb-2 px-2 py-1 bg-rose-300 text-white rounded-t-lg">
-            Jugadores
-          </h3>
-          <ul class="px-2 py-1">
-            {userCorrectWords.map((userCorrectWords, key) => (
-              <li key={key}>
-                <strong>{userCorrectWords.name}</strong>
-                {userCorrectWords.score}
-              </li>
-            ))}
-          </ul>
-        </div>
         <div
           className="absolute inset-0 z-[-1] bg-cover bg-center"
           style={{
             backgroundImage: "url('../style/spinning-bg-pinchitos.png')",
           }}
         ></div>
-        <div className="h-64 px-4 py-12 mt-10 mx-auto border-4 border-rose-300 bg-rose-100 shadow-2xl rounded-lg">
-          <div className="flex items-center justify-center -mt-8">
-            <div
-              id="contador"
-              className="w-16 h-16 rounded-full bg-red-500 flex items-center justify-center text-white font-bold text-lg"
-            >
-              {timer}
-            </div>
-            {painter ? (
-              <>
-                <div className="absolute mt-44 ml-96 underline font-bold">
-                  PALABRA:
-                </div>
-              </>
-            ) : (
-              <></>
-            )}
-          </div>
-          <div className="absolute mt-44 ml-96 underline font-bold">
-            Round: {round} / 3
-          </div>
-          {painter ? (
-            <>
+        <div className="flex mt-10 ">
+          <div class="-ml-5 h-44 w-96 absolute p-3 mx-4 bg-white border-4 border-rose-300 shadow-2xl rounded-lg">
+        {painter ? (
+            <div id="colores" className="w-full">
               <h2 className="text-2xl font-bold tracking-tight text-gray-900 mb-6">
                 Choose your colors
                 <button
@@ -270,78 +243,104 @@ export const PlayGame = ({ socket }) => {
                 <input
                   type="range"
                   min="1"
-                  max="30"
+                  max="20"
                   id="brushSize"
                   className="h-4 w-48"
                   defaultValue="3"
                 />
-                <span className="ml-4 text-gray-700">{brushSize}</span>
+                <span className="ml-4 text-gray-700">{brushSize}</span> 
+              </div> 
+            </div> )
+                : <></> }
+            {/* <!-- JUGADORES --> */}
+            <br></br>
+            <div className="w-96 h-full -mt-1 -ml-4 absolute bg-white border-4 border-rose-500 rounded-lg">
+              <h3 class="text-lg text-center font-bold mb-2 px-2 py-1 bg-rose-300 text-white border-4 border-rose-300 ">
+                Players
+              </h3>
+              <ul class="px-2 py-1">
+                {userCorrectWords.map((userCorrectWords, key) => (
+                  <li key={key}>
+                    <strong>{userCorrectWords.name}</strong>
+                    {userCorrectWords.score}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="h-36 w-4/6 ml-96 px-4 py-12 mx-auto border-4 border-rose-300 bg-rose-100 shadow-2xl rounded-lg">
+            <div className="flex items-center justify-center -mt-8">
+              <div
+                id="contador"
+                className="w-16 h-16 rounded-full bg-red-500 flex items-center justify-center text-white font-bold text-lg"
+              >
+                {timer}
               </div>
-              {word}
-            </>
-          ) : (
-            <></>
-          )}
+              {painter ? (
+                <div className="absolute mt-28 ">
+                  <h2 className="underline font-bold">{word}</h2>
+                </div>
+              ) : (
+                <></>
+              )}
+              <div className="absolute -mt-10 ml-96 underline font-bold">
+                Round: {round} / 3
+              </div>
+            </div>
+          </div>
         </div>
         <div className="mt-5 mb-5 flex">
           <div id="canvas" className="flex items-center justify-center">
             <canvas
               ref={canvasRef}
-              width="700px"
+              width="633px"
               height="600px"
-              className="bg-white border-4 border-rose-300"
+              className="ml-96 mx-auto bg-white border-4 border-pink-500"
             ></canvas>
           </div>
           {/* CHAT */}
-          <div id="chat" className="m-auto w-96 ml-5 -mt-0.5">
-            <div className="h-96 overflow-y-scroll border-4 border-rose-300 bg-white p-4">
-              <div className="flex flex-col gap-2">
-                <div className="flex flex-col items-end">
-                  <ul>
-                    {userWords.map((userWords, key) => (
-                      <li key={key}>
-                        <strong>{userWords.name}</strong>
-                        {userWords.word}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
+          <div id="chat" className="m-auto w-96 h-40 ml-5 -mt-48">
+            <div className="h-96 overflow-y-scroll border-4 border-pink-500 bg-white p-4">
+              <ul>
+                {userWords.map((userWords, key) => (
+                  <li key={key}>
+                    <strong>{userWords.name}</strong>
+                    {userWords.word}
+                  </li>
+                ))}
+              </ul>
             </div>
-            {painter ? (
-              <></>
-            ) :
-              wordCorrect ? (
-                <div className="flex gap-4 mt-4">
+            <div className="flex gap-4 mt-2">
+              {painter ? (
+                <></>
+              ) : wordCorrect ? (
+                <input
+                  name="word"
+                  type="text"
+                  value={wordInserted}
+                  onChange={(e) => setWordInserted(e.target.value)}
+                  className="border-2 border-gray-300 p-2 flex-1"
+                  placeholder="Escribe un mensaje"
+                />
+              ) : (
+                <form onSubmit={handleSubmit}>
                   <input
-                    type="text"
-                    className="border-2 border-gray-300 p-2 flex-1"
                     name="word"
+                    type="text"
                     value={wordInserted}
                     onChange={(e) => setWordInserted(e.target.value)}
+                    className="border-2 border-gray-300 p-2 flex-1"
+                    placeholder="Escribe un mensaje"
                   />
-                </div>
-              ) : (
-                <div className="flex gap-4 mt-4">
-                  <form onSubmit={handleSubmit}>
-                    <input
-                      type="text"
-                      placeholder="Introduce Word"
-                      className="border-2 border-gray-300 p-2 flex-1"
-                      name="word"
-                      value={wordInserted}
-                      onChange={(e) => setWordInserted(e.target.value)}
-                    />
-                    <button
-                      className="bg-rose-500 text-white px-4 py-2 rounded-lg hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
-                      type="submit"
-                    >
-                      Enviar
-                    </button>
-                  </form>
-                </div>
-              )
-            }
+                  <button
+                    type="submit"
+                    className="bg-rose-500 text-white px-4 py-2 rounded-lg hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-500"
+                  >
+                    Enviar
+                  </button>
+                </form>
+              )}
+            </div>
           </div>
         </div>
       </div>
