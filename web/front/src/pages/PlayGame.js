232bebdf8e6682a073e76d305b7fe30f44d387f2
Arnau Català <a22arncatsan@inspedralbes.cat>
2023-03-22 12:42:25 +0100

    console i warnings

--- web/front/src/pages/PlayGame.js
+++ web/front/src/pages/PlayGame.js
@@ -363,7 +363,7 @@ export const PlayGame = ({ socket }) => {
                   onChange={(e) => setWordInserted(e.target.value)}
                   className="w-64 border-2 border-gray-300 p-2 flex-1"
                   placeholder="Type the word"
-                  autocomplete="off"
+                  autoComplete="off"
                 />
               ) : (
                 <form onSubmit={handleSubmit} className="flex items-center">
@@ -374,7 +374,7 @@ export const PlayGame = ({ socket }) => {
                     onChange={(e) => setWordInserted(e.target.value)}
                     className="inline w-40 border-2 border-gray-300 p-2 flex-1"
                     placeholder="Type the word"
-                    autocomplete="off"
+                    autoComplete="off"
                   />
                   <button
                     type="submit"
