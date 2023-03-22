75ce32ef4ecba0c14ada417df191c40b5b77843a
CarlosGomezFuentes <a21cargomfue@inspedralbes.cat>
2023-03-22 12:24:44 +0100

    Diseño básico ranking

--- web/front/src/pages/RankingGame.js
+++ web/front/src/pages/RankingGame.js
@@ -6,22 +6,33 @@ export const RankingGame = () => {
     const [scoreSort, setScoreSort] = useState([])
 
     useEffect(() => {
-        let test = dataScoreBoard.scoreBoard
+        // let test = dataScoreBoard.scoreBoard
+
+        const scoreBoard = [
+            {userId: 1, name: 'carlos', score: 100},
+            {userId: 2, name: 'arnau', score: 300},
+            {userId: 3, name: 'isma', score: 400}
+        ]
     
-        console.log([...test].sort((a, b) => b.score - a.score))
-        const scoreList  = [...test].sort((a, b) => b.score - a.score)
-        console.log(scoreList)
-        setScoreSort(scoreList);        
+        // console.log([...test].sort((a, b) => b.score - a.score))
+        const scoreList  = scoreBoard.sort((a, b) => b.score - a.score)
+        // console.log(scoreList)
+        setScoreSort(scoreBoard);        
     }, [dataScoreBoard])
   
     
-    return <ul>
-    {scoreSort.map((player) => (      
-          <li key={player.userId}>
-            <span>{player.name}</span>
-            <span>{player.score}</span>
-          </li>
-    ))}
-    </ul>;
+    return (
+        <div>
+            <ul className="list-none p-0;">
+                {scoreSort.map((player, index) => (
+                <li key={player.userId} className="flex justify-between align-center p-10 border-b-2 ">
+                    <span>{index+1}.</span>
+                    <span>{player.name}</span>
+                    <span>{player.score}</span>
+                </li>
+                ))}
+            </ul>
+        </div>
+    )
   };
   
