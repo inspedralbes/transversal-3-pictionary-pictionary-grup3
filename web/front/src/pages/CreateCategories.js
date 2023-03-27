bea1c41116282fd9db2e3cfcc433a03c0ff02249
a21ismalgelo <a21ismalgelo@inspedralbes.cat>
2023-03-27 13:39:58 +0200

    disseny createCategories

--- web/front/src/pages/CreateCategories.js
+++ web/front/src/pages/CreateCategories.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 export const CreateCategories = () => {
   const stateLoginToken = useSelector((state) => state.login.loginToken);
@@ -27,28 +28,57 @@ export const CreateCategories = () => {
     }
   };
 
+  function categoriaCreada() {
+    Swal.fire({
+      title: "Category created!",
+      text: "The category has been created successfully",
+      icon: "success",
+      confirmButtonText: "Ok",
+    });
+    setCreated(false);
+  }
+
   return (
     <div className="flex items-center h-screen bg-cover bg-center w-screen bg-[url('../style/spinning-bg-only-pinchitos.png')]">
       <div className="mx-auto max-w-2xl">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
         <div className="text-center ">
           <div className="bg-rose-100 lg:h-auto lg:w-[32rem] opacity-70 lg:rounded-lg mx-auto lg:m-[auto] p-6 block h-screen w-screen">
-            <Link to="/categories">Back</Link>
+            <Link to="/categories">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+                />
+              </svg>
+            </Link>
             <form className="mt-8 space-y-6" action="#" onSubmit={handleSubmit}>
-              <label>Set the category title</label>
+              <label for="title-category" className="text-lg font-semibold">
+                Set the category title:{" "}
+              </label>
               <input
                 type="text"
+                id="title-category"
                 value={titleCategories}
                 onChange={(e) => setTitleCategories(e.target.value)}
+                className="w-6/12 mx-[auto] mt-0 input-join outline outline-1 focus:outline-2 focus:outline-rose-500 bg-opacity-1 bg-slate-50"
               ></input>
               <button
                 type="submit"
-                className="rounded-lg w-24 p-1.5 m-1 outline outline-2 outline-orange-500 text-gray-900 hover:pink-to-orange-gr hover:outline-none hover:text-rose-50 font-semibold"
+                className="rounded-lg w-auto p-1.5 m-1 outline outline-2 outline-orange-500 text-gray-900 hover:pink-to-orange-gr hover:outline-none hover:text-white font-semibold"
               >
                 Create Category
               </button>
             </form>
-            {created ? <h2>La categoria se ha creado correctamente</h2> : <></>}
+            {created ? categoriaCreada() : <></>}
           </div>
         </div>
       </div>
--- web/front/src/pages/LandingPage.js
+++ web/front/src/pages/LandingPage.js
@@ -1,31 +1,38 @@
-import logoSmall from '../style/logoPictoboomSmall.png';
-import '../style/landingStyle.css';
-import { Link } from 'react-router-dom';
+import logoSmall from "../style/logoPictoboomSmall.png";
+import "../style/landingStyle.css";
+import { Link } from "react-router-dom";
 
 export const LandingPage = () => {
   return (
     <div className="flex items-center h-screen bg-cover bg-center w-screen bg-[url('../style/spinning-bg-only-pinchitos.png')]">
-      <div className='mx-auto max-w-2xl'>
-        <div className='hidden sm:mb-8 sm:flex sm:justify-center'></div>
-        <div className='text-center '>
-          <img className='lg:w-96 md:w-96 w-72 m-[auto]' src={logoSmall} alt='Logo' />
-          <p className='mt-6 mx-4 lg:m-0 text-lg leading-8 text-slate-700 font-semibold'>
+      <div className="mx-auto max-w-2xl">
+        <div className="hidden sm:mb-8 sm:flex sm:justify-center"></div>
+        <div className="text-center ">
+          <img
+            className="lg:w-96 md:w-96 w-72 m-[auto]"
+            src={logoSmall}
+            alt="Logo"
+          />
+          <p className="mt-6 mx-4 lg:m-0 text-lg leading-8 text-slate-700 font-semibold">
             ¡PictoBoom, where your drawings come alive!<br></br>
           </p>
-          <div className='mt-10 block gap-x-6 relative group'>
-            <div className='absolute inset-2 bottom-5 bg-gray-100 w-[95%] blur-2xl h-28 animate-bounce opacity-75  group-hover:opacity-100 transition duration-200'></div>
+          <div className="mt-10 block gap-x-6 relative group">
+            <div className="absolute inset-2 bottom-5 bg-gray-100 w-[95%] blur-2xl h-28 animate-bounce opacity-75  group-hover:opacity-100 transition duration-200"></div>
             <Link
-              to='/joinGame'
-              className='pincel relative mx-6 default-button block lg:m-4 text-2xl font-extrabold text-orange-600 hover:text-white shadow-2xl hover:pink-to-orange-gr hover:outline-gray-100 outline-orange-600 animate-bounce p-12'
+              to="/joinGame"
+              className="pincel relative mx-6 default-button block lg:m-4 text-2xl font-extrabold text-orange-600 hover:text-white shadow-2xl hover:pink-to-orange-gr hover:outline-gray-100 outline-orange-600 animate-bounce p-12"
             >
               Play now!
             </Link>
-            <Link
-              to='/login'
-              className='text-sm font-semibold text-gray-900 default-button hover:bg-gray-900 hover:text-gray-100 hover:outline-none'
-            >
-              Are you a teacher?
-            </Link>
+
+            <div className="mt-6">
+              <Link
+                to="/login"
+                className="text-sm font-semibold text-gray-900 default-button hover:bg-gray-900 hover:text-gray-100 hover:outline-none"
+              >
+                Are you a teacher?
+              </Link>
+            </div>
           </div>
         </div>
       </div>
